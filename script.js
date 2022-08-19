dab73a583c0d28d34a562236b8c1e877030443d3
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-08-19 21:00:23 +0600

    money master app complete

---
+++ script.js
@@ -0,0 +1,75 @@
+//step-1: create a function for inputField value
+
+function inputFieldValue(inputField) {
+    const input = document.getElementById(inputField);
+    const inputValue = input.value;
+    const inpuNumber = parseFloat(inputValue);
+    input.value = "";
+    if (isNaN(inpuNumber)) {
+        alert("Please give valid amount");
+        return inputFieldValue();
+    }
+    else {
+        return inpuNumber;
+    }
+}
+
+// step-7: create a function for innerText value
+function innerTextValue(text, value) {
+    const stringText = document.getElementById(text);
+    stringText.innerText = value;
+}
+
+//step-2: add a handler in the calculate button for calculation
+
+const calculation = document.getElementById("calculate");
+calculation.addEventListener("click", function () {
+    //step-3: get income value
+    const income = inputFieldValue("income");
+    //step-4: get food-expenses value
+    const food = inputFieldValue("food-expenses");
+    //step-5: get rent-expenses value
+    const rent = inputFieldValue("rent-expenses");
+    //step-6: get clothes-expenses value
+    const clothes = inputFieldValue("clothes-expenses");
+    //step-7:calculate income and expenses amount
+    const Expenses = food + rent + clothes;
+    const Blance = income - Expenses;
+
+
+    if (income < 0 || food < 0 || rent < 0 || clothes < 0) {
+        alert("Please give positive input")
+    } else {
+        //step-9: show totalExpenses amount
+        innerTextValue("total-expenses", Expenses.toFixed(2));
+        //step-10: show totalBlance amount
+        innerTextValue("total-blance", Blance.toFixed(2));
+    }
+});
+
+
+//step-11: add handler in the save button to get save parcentage
+
+const save = document.getElementById("save-percentag");
+save.addEventListener("click", function () {
+    // step-12: get percentage input value
+    const input_percentage = inputFieldValue("input-percentag");
+    //step-13:get total blance
+
+    const blance = document.getElementById("total-blance");
+    const blanceValue = blance.innerText;
+    const blanceAmount = parseFloat(blanceValue);
+
+    //step-14: calculate percentage amount
+    const percentag = (blanceAmount * input_percentage) / 100;
+
+    //step-15 get remainder Blance
+    const remainderBlance = blanceValue - percentag;
+
+    //step-16: showing saving and remainder amount
+    innerTextValue("saving-amount", percentag.toFixed(2));
+    innerTextValue("reminder-blance", remainderBlance.toFixed(2));
+    console.log(percentag);
+});
+
+
